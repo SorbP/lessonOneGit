e12d0c0b085398cd13de05e36a501f42fe81d485
Stefan Persson <sorbpen@gmail.com>
2020-06-11 18:20:30 +0200

    First Commit

---
+++ LessonOne/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace LessonOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array1 = new int[3];
+            string line = Console.ReadLine();
+            int value;
+            value = Convert.ToInt32(line);
+            array1[0] = value;
+            line = Console.ReadLine();
+
+            value = Convert.ToInt32(line);
+            array1[1] = value;
+            value = Convert.ToInt32(line);
+            line = Console.ReadLine();
+            
+            value = Convert.ToInt32(line);
+            array1[2] = value;
+            value = Convert.ToInt32(line);
+            line = Console.ReadLine();
+
+
+            Array.ForEach(array1, Console.WriteLine);
+
+            Array.Sort(array1);
+
+            Array.ForEach(array1, Console.WriteLine);
+
+
+            Console.ReadLine();
+        }
+    }
+}
